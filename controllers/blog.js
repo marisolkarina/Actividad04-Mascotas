9230f09b89100cb061200d609ea520250226aca9
marisolkarina <marisol.kari.20@gmail.com>
2024-11-16 23:59:29 -0500

    editar publicacion si es admin o autor

--- controllers/blog.js
+++ controllers/blog.js
@@ -10,7 +10,8 @@ exports.getPublicaciones = (req, res) => {
                 publicaciones: publicaciones,
                 titulo: "Blog", 
                 path: "/blog",
-                autenticado: req.session.autenticado
+                autenticado: req.session.autenticado,
+                usuario: req.usuario
             });
         }).catch((err) => {
             console.log(err);
@@ -46,6 +47,49 @@ exports.postCrearPublicacion = (req, res) => {
         .save()
         .then((result) => {
             console.log('Publicacion creada');
+            res.redirect('/blog');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+exports.getEditarPublicacion = (req, res) => {
+    const idPublicacion = req.params.idPublicacion;
+
+    Publicacion.findById(idPublicacion)
+        .then((publicacion) => {
+            
+            res.render('blog/crear-editar-publicacion', {
+                titulo: 'Editar Producto',
+                path: '/editar-publicacion',
+                publicacion: publicacion,
+                modoEdicion: true,
+                autenticado: req.session.autenticado
+            })
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+exports.postEditarPublicacion = (req, res, next) => {
+    const idPublicacion = req.body.idPublicacion;
+    const titulo = req.body.titulo;
+    const urlImagen = req.body.urlImagen;
+    const descripcion = req.body.descripcion;
+
+    Publicacion.findById(idPublicacion)
+        .then((publicacion) => {
+
+            // en la vista esta la condicion para editar si es admin o autor de la publicacion
+            publicacion.titulo = titulo;
+            publicacion.urlImagen = urlImagen;
+            publicacion.descripcion = descripcion;
+            
+            return publicacion.save();
+
+        })
+        .then((result) => {
             res.redirect('/blog');
         })
         .catch((err) => {
--- routes/blog.js
+++ routes/blog.js
@@ -6,7 +6,11 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/blog', blogController.getPublicaciones);
+
 router.get('/crear-publicacion', isAuth, blogController.getCrearPublicacion);
 router.post('/crear-publicacion', isAuth, blogController.postCrearPublicacion);
+
+router.get('/editar-publicacion/:idPublicacion', isAuth, blogController.getEditarPublicacion);
+router.post('/editar-publicacion', isAuth, blogController.postEditarPublicacion);
 
 module.exports = router;
