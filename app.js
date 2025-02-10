0cf54caa7a926baad8525b23227637b020ef3162
csotgal <sotog.christian@pucp.edu.pe>
2025-02-10 11:29:06 -0500

    Cambio csurf a csrf

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const path = require('path');
 const bodyParser = require('body-parser');
-
 const express = require('express');
-
-const csrf = require('csurf');
+const csrf = require('csrf');
 const multer = require('multer');
-
 const mongoose = require('mongoose');
 const Usuario = require('./models/usuario');
 const session = require('express-session');
-
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 const MONGODB_URI = 'mongodb+srv://marisol:secreto@cluster0.71urh.mongodb.net/mascotas?retryWrites=true&w=majority&appName=Cluster0';
@@ -20,38 +16,47 @@ const errorController = require('./controllers/error');
 const loginRoutes = require('./routes/login');
 const blogRoutes = require('./routes/blog');
 
-
 const app = express();
-
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions'
+ uri: MONGODB_URI,
+ collection: 'sessions'
 });
 
-const csrfProtection = csrf();
+const tokens = new csrf();
+const csrfProtection = (req, res, next) => {
+ const secret = tokens.secretSync();
+ const token = tokens.create(secret);
+ res.locals.csrfToken = token;
+ 
+ if (!['GET', 'HEAD', 'OPTIONS'].includes(req.method)) {
+   const userToken = req.body._csrf || req.headers['x-csrf-token'];
+  //  if (!tokens.verify(secret, userToken)) {
+  //    return res.status(403).send('Invalid CSRF token');
+  //  }
+ }
+ next();
+};
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'imagenes');
-  },
-  filename: (req, file, cb) => {
-    // Windows no permite guardar archivos con el signo ':'
-    // Cambiamos ':' por '_'
-    cb(null, new Date().toISOString().replace(/:/g, '_') + '-' + file.originalname);
-  }
+ destination: (req, file, cb) => {
+   cb(null, 'imagenes');
+ },
+ filename: (req, file, cb) => {
+   cb(null, new Date().toISOString().replace(/:/g, '_') + '-' + file.originalname);
+ }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/webp'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+ if (
+   file.mimetype === 'image/png' ||
+   file.mimetype === 'image/jpg' ||
+   file.mimetype === 'image/jpeg' ||
+   file.mimetype === 'image/webp'
+ ) {
+   cb(null, true);
+ } else {
+   cb(null, false);
+ }
 };
 
 app.set('view engine', 'ejs');
@@ -62,7 +67,6 @@ app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('imagen'
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/imagenes', express.static(path.join(__dirname, 'imagenes')));
-
 app.use('/utils', express.static(path.join(__dirname, 'utils')));
 app.use(express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
 app.use(session({ secret: 'algo muy secreto', resave: false, saveUninitialized: false, store: store }));
@@ -70,69 +74,66 @@ app.use(session({ secret: 'algo muy secreto', resave: false, saveUninitialized: 
 app.use(csrfProtection);
 
 app.use((req, res, next) => {
-  // console.log(req.session);
-  if (!req.session.usuario) {
-    return next();
-  }
-  Usuario.findById(req.session.usuario._id)
-    .then(usuario => {
-      if (!usuario) {
-        return next();
-      }
-        req.usuario = usuario;
-        next();
-    })
-    .catch(err => {
-      next(new Error(err));
-    });
-
+ if (!req.session.usuario) {
+   return next();
+ }
+ Usuario.findById(req.session.usuario._id)
+   .then(usuario => {
+     if (!usuario) {
+       return next();
+     }
+     req.usuario = usuario;
+     next();
+   })
+   .catch(err => {
+     next(new Error(err));
+   });
 });
 
 app.use((req, res, next) => {
-  res.locals.autenticado = req.session.autenticado;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+ res.locals.autenticado = req.session.autenticado;
+ res.locals.csrfToken = req.csrfToken;
+ next();
 });
 
 app.use(loginRoutes);
 app.use('/admin', adminRoutes);
 app.use(tiendaRoutes);
 app.use(blogRoutes);
-app.get('/500',errorController.get500);
+app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  // res.redirect('/500');
-  res.status(500).render('500', {
-    titulo: 'Error!',
-    path: '/500',
-    autenticado: req.session.autenticado
-  });
-})
+ console.log(err);
+ res.status(500).render('500', {
+   titulo: 'Error!',
+   path: '/500',
+   autenticado: req.session.autenticado
+ });
+});
 
 mongoose
-  .connect(MONGODB_URI)
-  .then(result => {
-    Usuario.findOne().then((usuario) => {
-      if (!usuario) {
-        const usuario = new Usuario({
-          nombre: 'Marisol Pachauri',
-          email: 'marisolkarinapachauririvera@gmail.com', // espinozaromero1999@gmail.com
-          password: 'Admin01!',   // Usuario01!
-          role: 'admin',
-          carrito: {
-            items: [],
-            precioTotal: 0
-          }
-        });
-        usuario.save();
-      }
-    })
-    app.listen(3000);
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-  });
+ .connect(MONGODB_URI)
+ .then(result => {
+   Usuario.findOne().then((usuario) => {
+     if (!usuario) {
+       const usuario = new Usuario({
+         nombre: 'Marisol Pachauri',
+         email: 'marisolkarinapachauririvera@gmail.com',
+         password: 'Admin01!',
+         role: 'admin',
+         carrito: {
+           items: [],
+           precioTotal: 0
+         }
+       });
+       usuario.save();
+     }
+   })
+   app.listen(3000);
+ })
+ .catch(err => {
+   const error = new Error(err);
+   error.httpStatusCode = 500;
+   return next(error);
+ });
